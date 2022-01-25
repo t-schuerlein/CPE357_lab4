d911b40c2a4e0f650e20848393838cf3ad2d7021
crowdedpoem <tasschuerlein@yahoo.com>
2022-01-24 21:19:41 -0800

    brightness bad and need to time

--- lab4.cpp
+++ lab4.cpp
@@ -188,27 +188,27 @@ else{
 
 
     // parent code here
-    // for(int y = 0; y < split; y ++){
-    //         for(int x = 0; x < piWidth; x++){
-    //             Byte g = get_green(idata, x, y, width, size);
-    //             Byte b = get_blue(idata, x, y, width, size);
-    //             Byte r = get_red(idata, x, y,width, size);
+    for(int y = 0; y < split; y++){
+            for(int x = 0; x < piWidth; x++){
+                Byte g = get_green(idata, x, y, width, size);
+                Byte b = get_blue(idata, x, y, width, size);
+                Byte r = get_red(idata, x, y,width, size);
 
-    //             g += (int)(brightness * 255);
-    //             b += (int) (brightness * 255);
-    //             r += (int) (brightness * 255);
+                g += (int)(brightness * 255);
+                b += (int) (brightness * 255);
+                r += (int) (brightness * 255);
 
-    //             g = min(g, (Byte) 255);
-    //             b = min(b, (Byte) 255);
-    //             r = min(r, (Byte) 255);
+                g = min(g, (Byte) 255);
+                b = min(b, (Byte) 255);
+                r = min(r, (Byte) 255);
 
-    //             p[(x*3)+0 + y * width] = g;
-    //             p[(x*3)+1 + y * width] = b ;
-    //             p[(x*3)+2 + y * width] = r;
+                p[(x*3)+0 + y * width] = g;
+                p[(x*3)+1 + y * width] = b ;
+                p[(x*3)+2 + y * width] = r;
 
 
-    //         }
-    //     }
+            }
+        }
 
     cout << "parent tingz" << endl;
 
